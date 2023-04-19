f6c5ad207a35c24645bf8af16b5c54a0adee172f
jaheemprevost <jaheemdp@gmail.com>
2023-04-18 21:21:56 -0400

    Implement api/dom logic

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -1,8 +1,22 @@
 
-export default function CharacterCard(props) {
+export default function CharacterCard({characterData}) {
   return (
     <div className="character-card">
-
+      <img className="character-img" src={characterData.photoUrl} alt={characterData.name}/>
+      <div className="character-info">
+        <p><span>Name:</span> {characterData.name || 'N/A'}</p>
+        <p><span>Gender:</span> {characterData.gender || 'N/A'}</p>
+        <p><span>Eye Color:</span> {characterData.eye || 'N/A'}</p>
+        <p><span>Hair Color:</span> {characterData.hair || 'N/A'}</p>
+        <p><span>Skin Color:</span> {characterData.skin || 'N/A'}</p>
+        <p><span>Weapon:</span> {characterData.weapon || 'N/A'}</p>
+        <p><span>Allies:</span> {characterData.allies.join(', ')}</p>
+        <p><span>Enemies:</span> {characterData.enemies.join(', ')}</p>
+        <p><span>Profession:</span> {characterData.profession || 'N/A'}</p>
+        <p><span>Position:</span> {characterData.position || 'N/A'}</p>
+        <p><span>Predecessor:</span> {characterData.predecessor || 'N/A'}</p>
+        <p><span>Affiliation:</span> {characterData.affiliation || 'N/A'}</p>
+      </div>
     </div>
   )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,54 @@
 import {useState, useEffect} from 'react';
 import CharacterCard from './CharacterCard';
+
 export default function Home() {
-  const [characterData, setCharacterData] = useState(null);
+  const [characterId, setCharacterId] = useState(null);
+  const [character, setCharacter] = useState(null);
+  const [query, setQuery] = useState('');
+
+  function searchQuery(e) {
+    const { value } = e.target;
+
+    if (e.key === "Enter") {
+      setQuery(value);
+    }
+  }
+
+  useEffect( () => {
+    const fetchCharacters = async () => {
+      const res = await fetch('https://last-airbender-api.fly.dev/api/v1/characters?perPage=497');
+      const characters = await res.json();
+      const character = characters.find(character => {
+        const name = character.name.toLowerCase();
+        return name.includes(query.toLowerCase());
+      });
+      setCharacterId(character._id);
+    };
+    if (query) {
+      fetchCharacters(); 
+    }
+  }, [query]);   
+  
+  useEffect(() => {
+    if (!characterId) {
+      return;
+    }
+
+    const fetchCharacter = async () => {
+      const res = await fetch(`https://last-airbender-api.fly.dev/api/v1/characters/${characterId}`);
+      const character = await res.json();
+      setCharacter(character);
+    };
+
+    fetchCharacter();
+  }, [characterId]);
   return (
     <main className='main'>
-      <input type='search' placeholder='Search for a character...'/>
+      <input className="search-field" onKeyDown={e => searchQuery(e)} type='search' placeholder='Search for a character...'/>
+
+      {character ? 
+      <CharacterCard characterData={character} /> 
+      : <p>No Character Found</p>}
     </main>
   )
 }
